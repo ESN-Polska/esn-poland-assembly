d477118620c5899c4ea5fdfb81ce8fc27dba29ba
Szymon Cichy <dev@szycic.com>
2026-09-19 20:48:38 +0200

    Optimise releasing new front-end env to AWS

--- front-end/release.sh
+++ front-end/release.sh
@@ -59,7 +59,7 @@ BUCKET=`aws cloudfront get-distribution --id ${DISTRIBUTION} --profile ${AWS_PRO
 
 # upload the project's files to the S3 bucket
 echo -e "${C}Uploading...${NC}"
-aws s3 sync ./www s3://${BUCKET} --profile ${AWS_PROFILE} --delete --exclude ".well-known/*" 1>/dev/null
+aws s3 sync ./www s3://${BUCKET} --profile ${AWS_PROFILE} --delete --size-only --exclude ".well-known/*" 1>/dev/null
 
 # invalidate old common files from the CloudFront distribution
 echo -e "${C}Cleaning...${NC}"
